e53bb593028322367ebfd4b6009175ca6b21a8ec
Ali-Qadir <aliqadir786.is@gmail.com>
2023-09-12 21:07:46 +0530

    Add Synced Player Movements

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private float _speed = 5f;
+
+    private PhotonView _photonView;
+    private void Start()
+    {
+        _photonView = GetComponent<PhotonView>();
+    }
+
+    private void Update()
+    {
+        if (!_photonView.IsMine) return;
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        var direction = new Vector3(horizontal, 0, vertical).normalized;
+        _controller.Move((_speed * Time.deltaTime) * direction);
+    }
+}
